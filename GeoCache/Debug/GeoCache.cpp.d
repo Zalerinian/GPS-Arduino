3093ac9b1037f51676ff198125580fa4811eb2c4
Drew Ritter <dritter@razelon.com>
2016-09-06 19:06:41 -0400

    Initial Commit

---
+++ GeoCache/Debug/GeoCache.cpp.d
@@ -0,0 +1,25 @@
+C:\Users\Drew\AppData\Local\Temp\VMicroBuilds\GeoCache\uno\GeoCache.cpp.o: \
+ C:\Users\Drew\AppData\Local\Temp\VMicroBuilds\GeoCache\uno\GeoCache.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\tf0qmlza.e5u\Micro\ Platforms\default\debuggers/VM_DBG/VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/SD.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFat.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2Card.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdInfo.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/FatStructs.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFatUtil.h
