3a8b15cdb15966314252024d266ec7d4e15e1433
Zixun Xiang <zxiang@fullsail.edu>
2016-09-21 15:15:31 -0700

    add debounce function

--- GeoCache/Debug/GeoCache.cpp.d
+++ GeoCache/Debug/GeoCache.cpp.d
@@ -1,23 +1,23 @@
-C:\Users\Logano\AppData\Local\Temp\VMicroBuilds\GeoCache\uno\GeoCache.cpp.o: \
- C:\Users\Logano\AppData\Local\Temp\VMicroBuilds\GeoCache\uno\GeoCache.cpp \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- e:\arudio\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Users\Logano\Documents\Arduino\libraries\Adafruit_NeoPixel-master/Adafruit_NeoPixel.h \
- e:\arudio\Arduino\libraries\SD\src/SD.h \
- e:\arudio\Arduino\libraries\SD\src/utility/SdFat.h \
- e:\arudio\Arduino\libraries\SD\src/utility/Sd2Card.h \
- e:\arudio\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
- e:\arudio\Arduino\libraries\SD\src/utility/SdInfo.h \
- e:\arudio\Arduino\libraries\SD\src/utility/FatStructs.h \
- e:\arudio\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- e:\arudio\Arduino\libraries\SD\src/utility/SdFatUtil.h
+C:\Users\oriharan40\AppData\Local\Temp\VMicroBuilds\GeoCache\uno\GeoCache.cpp.o: \
+ C:\Users\oriharan40\AppData\Local\Temp\VMicroBuilds\GeoCache\uno\GeoCache.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\oriharan40\Documents\Arduino\libraries\Adafruit_NeoPixel-1.0.6/Adafruit_NeoPixel.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/SD.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFat.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2Card.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/Sd2PinMap.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdInfo.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/FatStructs.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\SD\src/utility/SdFatUtil.h
